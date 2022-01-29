adc8c5bfc710f10486d66da8877a57f471d171d8
berezkin88 <berezkin05@gmail.com>
2022-01-29 13:26:04 +0200

    added address embadded entity

--- src/main/java/com/example/orderservice/domain/OrderHeader.java
+++ src/main/java/com/example/orderservice/domain/OrderHeader.java
@@ -1,14 +1,50 @@
 package com.example.orderservice.domain;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
-public class OrderHeader extends BaseEntity{
+@AttributeOverride(
+    name = "shippingAddress.address",
+    column = @Column(name = "shipping_address")
+)
+@AttributeOverride(
+    name = "shippingAddress.city",
+    column = @Column(name = "shipping_city")
+)
+@AttributeOverride(
+    name = "shippingAddress.state",
+    column = @Column(name = "shipping_state")
+)
+@AttributeOverride(
+    name = "shippingAddress.zipCode",
+    column = @Column(name = "shipping_zip_code")
+)
+@AttributeOverride(
+    name = "billToAddress.address",
+    column = @Column(name = "bill_to_address")
+)
+@AttributeOverride(
+    name = "billToAddress.city",
+    column = @Column(name = "bill_to_city")
+)
+@AttributeOverride(
+    name = "billToAddress.state",
+    column = @Column(name = "bill_to_state")
+)
+@AttributeOverride(
+    name = "billToAddress.zipCode",
+    column = @Column(name = "bill_to_zip_code")
+)
+public class OrderHeader extends BaseEntity {
 
     private String customerName;
+    @Embedded
+    private Address shippingAddress;
+    @Embedded
+    private Address billToAddress;
 
     public String getCustomerName() {
         return customerName;
@@ -16,6 +52,22 @@ public class OrderHeader extends BaseEntity{
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillToAddress() {
+        return billToAddress;
+    }
+
+    public void setBillToAddress(Address billToAddress) {
+        this.billToAddress = billToAddress;
     }
 
     @Override
@@ -26,13 +78,18 @@ public class OrderHeader extends BaseEntity{
 
         OrderHeader that = (OrderHeader) o;
 
-        return customerName != null ? customerName.equals(that.customerName) : that.customerName == null;
+        if (customerName != null ? !customerName.equals(that.customerName) : that.customerName != null) return false;
+        if (shippingAddress != null ? !shippingAddress.equals(that.shippingAddress) : that.shippingAddress != null)
+            return false;
+        return billToAddress != null ? billToAddress.equals(that.billToAddress) : that.billToAddress == null;
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + (customerName != null ? customerName.hashCode() : 0);
+        result = 31 * result + (shippingAddress != null ? shippingAddress.hashCode() : 0);
+        result = 31 * result + (billToAddress != null ? billToAddress.hashCode() : 0);
         return result;
     }
 }
